0d14097623e3bbd5906d4126733d592f7e3b20e9
Ed <ed@luno.com>
2022-12-08 17:48:43 +0000

    2022:Day 8:Part 2

--- 2022/Day8/main.go
+++ 2022/Day8/main.go
@@ -23,39 +23,36 @@ func treetopTreeHouse(filename string) (int, int, error) {
 	for x := 1; x < len(lines[0])-1; x++ {
 		for y := 1; y < len(lines)-1; y++ {
 			i := 0
+			score := 1
 			isVisible := false
 
 			// check for visibility to left
 			for i = x - 1; i > 0 && lines[y][i] < lines[y][x]; i-- {
 			}
 			isVisible = i == 0 && lines[y][i] < lines[y][x]
-			if isVisible {
-				visibleCount += 1
-				continue
-			}
+			score = score * (x - i)
 
 			// check for visibility to right
 			for i = x + 1; i < len(lines[y])-1 && lines[y][i] < lines[y][x]; i++ {
 			}
-			isVisible = i == len(lines[y])-1 && lines[y][i] < lines[y][x]
-			if isVisible {
-				visibleCount += 1
-				continue
-			}
+			isVisible = isVisible || i == len(lines[y])-1 && lines[y][i] < lines[y][x]
+			score = score * (i - x)
 
 			// check for visibility to top
 			for i = y - 1; i > 0 && lines[i][x] < lines[y][x]; i-- {
 			}
-			isVisible = i == 0 && lines[i][x] < lines[y][x]
-			if isVisible {
-				visibleCount += 1
-				continue
-			}
+			isVisible = isVisible || i == 0 && lines[i][x] < lines[y][x]
+			score = score * (y - i)
 
 			// check for visibility to bottom
 			for i = y + 1; i < len(lines)-1 && lines[i][x] < lines[y][x]; i++ {
 			}
-			isVisible = i == len(lines)-1 && lines[i][x] < lines[y][x]
+			isVisible = isVisible || i == len(lines)-1 && lines[i][x] < lines[y][x]
+			score = score * (i - y)
+
+			if score > highestScenicScore {
+				highestScenicScore = score
+			}
 			if isVisible {
 				visibleCount += 1
 			}
